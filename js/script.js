6d3bfd6ee7254b812629583bff379da5c181277d
Dony Cahyanto <you@example.com>
2025-10-25 03:06:03 +0700

    Finally

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+welcomeSpeech();
+
+
+function welcomeSpeech() {
+    let name = prompt("Masukkan nama Anda:");
+
+    document.getElementById("greet-name").innerHTML = `Hi ${name}`;
+}
+
+function validateForm() {
+    let name = document.getElementById("name-input").value.trim();
+    let date = document.getElementById("date-input").value;
+    let gender = document.querySelector("input[name='gender']:checked");
+    let message = document.getElementById("message-input").value;
+
+    if (name === "" || date === "" || !gender || message === "") {
+        alert("Harap isi semua kolom terlebih dahulu!");
+        return;
+    }
+
+    // Dapatkan waktu saat ini
+    let currentTime = new Date().toString();
+
+    // Tampilkan ke kotak hasil
+    document.getElementById("result-time").textContent = currentTime;
+    document.getElementById("result-name").textContent = name;
+    document.getElementById("result-date").textContent = date;
+    document.getElementById("result-gender").textContent = gender.value;
+    document.getElementById("result-message").textContent = message;
+
+    alert("Data berhasil ditampilkan!");
+}
